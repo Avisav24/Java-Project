a28a538447f42db1359f33b03313c65d53b9e745
Abhinav Vats <142833016+Avisav24@users.noreply.github.com>
2024-10-19 11:40:24 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,487 @@
 
+package Controller;
+
+import Database.DatabaseConnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AddDoctor")
+public class AddDoctor extends HttpServlet {
+
+    private int i;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            Date todaysDate = new Date();
+            DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+            String sid = request.getParameter("id");
+            int id = Integer.parseInt(sid);
+            String fname = request.getParameter("fname");
+            String lname = request.getParameter("lname");
+            String gender = request.getParameter("gender");
+            String phone = request.getParameter("Mobile");
+            String city = request.getParameter("City");
+            String email = request.getParameter("email");
+            String age = request.getParameter("age");
+            String address = request.getParameter("address");
+            String qualification = request.getParameter("qualification");
+
+            String DateAndTime = df2.format(todaysDate);
+
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("insert into doctor values(?,?,?,?,?,?,?,?,?,?,?)");
+            pst.setInt(1, id);
+            pst.setString(5, phone);
+            pst.setString(2, fname);
+            pst.setString(3, lname);
+            pst.setString(4, gender);
+            pst.setString(6, city);
+            pst.setString(7, email);
+            pst.setString(8, age);
+            pst.setString(9, address);
+            pst.setString(10, DateAndTime);
+            pst.setString(11, qualification);
+
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Data Add Successfully..!');");
+                pw.println("window.location.href = \"AdminHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AdminHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Failed !!!!,try Again Later!');");
+                pw.println("window.location.href = \"addDoctor.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("addDoctor.jsp");
+                //rd.forward(request, response);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(AddPatient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
+
+
+@WebServlet("/AddPatient")
+public class AddPatient extends HttpServlet {
+
+    private int i;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            Date todaysDate = new Date();
+            DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+            String fname = request.getParameter("fname");
+            String lname = request.getParameter("lname");
+            String gender = request.getParameter("gender");
+            String phone = request.getParameter("Mobile");
+            String city = request.getParameter("City");
+            String email = request.getParameter("email");
+            String age = request.getParameter("age");
+            String address = request.getParameter("address");
+
+            String DateAndTime = df2.format(todaysDate);
+
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("insert into patient values(?,?,?,?,?,?,?,?,?)");
+            pst.setString(9, phone);
+            pst.setString(1, fname);
+            pst.setString(2, lname);
+            pst.setString(3, gender);
+            pst.setString(4, city);
+            pst.setString(5, email);
+            pst.setString(6, age);
+            pst.setString(7, address);
+            pst.setString(8, DateAndTime);
+
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Login Successfully..!');");
+                pw.println("window.location.href = \"UserHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("UserHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Incorrect Data..!');");
+                pw.println("window.location.href = \"addpatient.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("addpatient.jsp");
+                //rd.forward(request, response);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AddPatient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AddPatient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
+
+
+@WebServlet("/AddRecp")
+public class AddRecp extends HttpServlet {
+
+    private int i;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            Date todaysDate = new Date();
+            DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+            String fname = request.getParameter("fname");
+            String lname = request.getParameter("lname");
+            String phone = request.getParameter("Mobile");
+            String DateAndTime = df2.format(todaysDate);
+
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("insert into recp values(?,?,?,?)");
+            pst.setString(1, fname);
+            pst.setString(2, lname);
+            pst.setString(3, phone);
+            pst.setString(4, DateAndTime);
+
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Add Successfully..!');");
+                pw.println("window.location.href = \"AdminHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AdminHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Incorrect Data...!');");
+                pw.println("window.location.href = \"AddRecp.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AddRecp.jsp");
+                //rd.forward(request, response);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(AddPatient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+@WebServlet("/AddWorker")
+public class AddWorker extends HttpServlet {
+
+    private int i;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+PrintWriter pw = response.getWriter();
+        try {
+            Date todaysDate = new Date();
+            DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+            String fname = request.getParameter("fname");
+            String lname = request.getParameter("lname");
+            String phone = request.getParameter("Mobile");
+            String DateAndTime = df2.format(todaysDate);
+
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("insert into worker values(?,?,?,?)");
+            pst.setString(1, fname);
+            pst.setString(2, lname);
+            pst.setString(3, phone);
+            pst.setString(4, DateAndTime);
+
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Add Successfully..!');");
+                pw.println("window.location.href = \"AdminHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AdminHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Incorrect Data..!');");
+                pw.println("window.location.href = \"AddWorker.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AddWorker.jsp");
+                //rd.forward(request, response);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(AddPatient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+
+@WebServlet("/AdminLogin")
+public class AdminLogin extends HttpServlet {
+
+    private String user;
+    private String pass;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            String userp = request.getParameter("your_name");
+            String passp = request.getParameter("your_pass");
+            Connection con = DatabaseConnection.initializeDatabase();
+
+            String s = "select *from adminreg";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(s);
+            while (rs.next()) {
+                user = rs.getString(1);
+                pass = rs.getString(2);
+            }
+            if (userp.equals(user) && passp.equals(pass)) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Login Successfully..!');");
+                pw.println("window.location.href = \"AdminHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AdminHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Username or Password is Incorrect..!');");
+                pw.println("window.location.href = \"index.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+                //rd.include(request, response);
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
+
+@WebServlet("/AdminRegister")
+public class AdminRegister extends HttpServlet {
+
+    private int i = 0;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            String userp = request.getParameter("email");
+            String passp = request.getParameter("pass");
+            String rpassp = request.getParameter("re_pass");
+            String tikbox = request.getParameter("agree-term");
+
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("insert into adminreg values(?,?)");
+            pst.setString(1, userp);
+            pst.setString(2, passp);
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Registerd Successfully..!');");
+                pw.println("window.location.href = \"adminLogin.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("adminLogin.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Username or Password is Incorrect..!');");
+                pw.println("window.location.href = \"adminRegister.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("adminRegister.jsp");
+                //rd.forward(request, response);
+            }
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
+
+
+
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+
+    private String user;
+    private String pass;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            String userp = request.getParameter("username");
+            String passp = request.getParameter("password");
+            Connection con = DatabaseConnection.initializeDatabase();
+
+            String s = "select *from login";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(s);
+            while (rs.next()) {
+                user = rs.getString(1);
+                pass = rs.getString(2);
+            }
+            if (userp.equals(user) && passp.equals(pass)) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Login Successfully..!');");
+                pw.println("window.location.href = \"UserHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("UserHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Username or Password is Incorrect..!');");
+                pw.println("window.location.href = \"index.jsp\";");
+                pw.println("</script>");
+                // RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+                // rd.include(request, response);
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
+
+@WebServlet("/UserRegister")
+public class UserRegister extends HttpServlet {
+
+    private String user;
+    private String pass;
+    private int i = 0;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            Connection con = DatabaseConnection.initializeDatabase();
+
+            user = request.getParameter("Username");
+            pass = request.getParameter("password");
+            String repassp = request.getParameter("repassword");
+
+            PreparedStatement pst = con.prepareStatement("insert into login values(?,?)");
+            pst.setString(1, user);
+            pst.setString(2, pass);
+            i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Register Successfully..!');");
+                pw.println("window.location.href = \"index.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Register Failed');");
+                pw.println("window.location.href = \"userRegister.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("userRegister.jsp");
+                //rd.forward(request, response);
+            }
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
+
+
+@WebServlet("/updatePatient")
+public class updatePatient extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String gender = request.getParameter("gender");
+        String phone = request.getParameter("Mobile");
+        String city = request.getParameter("City");
+        String email = request.getParameter("email");
+        String age = request.getParameter("age");
+        String address = request.getParameter("address");
+
+        try {
+            Connection con = DatabaseConnection.initializeDatabase();
+            PreparedStatement pst = con.prepareStatement("update patient set fname = ? , lname = ? , gender = ? , city = ? , email = ? , age = ? , address = ?  where mobile = '" + phone + "' ");
+            pst.setString(1, fname);
+            pst.setString(2, lname);
+            pst.setString(3, gender);
+            pst.setString(4, city);
+            pst.setString(5, email);
+            pst.setString(6, age);
+            pst.setString(7, address);
+
+            int i = pst.executeUpdate();
+            if (i > 0) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Update Successfully..!');");
+                pw.println("window.location.href = \"AdminHome.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("AdminHome.jsp");
+                //rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Failed..! Try Again Later...');");
+                pw.println("window.location.href = \"updatePatient.jsp\";");
+                pw.println("</script>");
+                //RequestDispatcher rd = request.getRequestDispatcher("updatePatient.jsp");
+                //rd.forward(request, response);
+            }
+            con.close();
+        } catch (Exception e) {
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
